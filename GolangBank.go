d4540402f198bf07e9ae5a54970429770d0509c6
JhansiTamminana <jhansi.tamminana25@gmail.com>
2024-11-15 02:18:12 +0530

    Create GolangBank.go

---
+++ GolangBank.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+fmt.Println("Welcome to Go Bank")
+fmt.Println("What do you want to do ?")
+fmt.Println("1.check balance")
+fmt.Println("2. Deposit money")
+fmt.Println("3 Withdraw  money")
+fmt.Println("4. Exit")
+
+var choice int
+fmt.Print"("your chioce : ")
+fmt.Scan(&choice)
+
+fmt.Println("your choice : ", choice)
+ fmt.Println("&choice "")
+}
